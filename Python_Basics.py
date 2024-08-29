8b830f95e0158efb24580c36ad1014bf2c9a599c
MukeshKethe <109723195+MukeshKethe@users.noreply.github.com>
2024-08-29 21:46:46 +0530

    Add files via upload

---
+++ Python_Basics.py
@@ -0,0 +1,25 @@
+#Assignment
+a = 10
+print(a)
+
+#Reassignment
+a = 20
+print(a)
+
+#Mutiple Assignment Method - 1
+a, b, c, d = 10, 20, 30, 40
+print(a)
+print(b)
+print(c)
+print(d)
+
+#Mutiple Assignment Method - 2
+a = b = c = d = 100
+print(a)
+print(b)
+print(c)
+print(d)
+
+#Deleting
+del a
+#print(a)
