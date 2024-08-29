73ff9bb55d972b12b1dbb84831d41d4e3e22c7bd
MukeshKethe <109723195+MukeshKethe@users.noreply.github.com>
2024-08-29 21:47:55 +0530

    Add files via upload

---
+++ Python_Variables_Naming_Rules.py
@@ -0,0 +1,11 @@
+#Variables
+#Naming Rules
+'''
+_a = 10 Possible
+@a = 10 Not Possible
+77a = 10 Not Possible
+if = 10 Not Possible
+a b = 10 Not Possiblee
+a_b = 10 Possible
+a77 = 10 Possible
+'''
